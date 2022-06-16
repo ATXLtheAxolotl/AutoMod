d20024f14a01b7814a47f642bc9e2a74fe8d6055
ATXLtheAxolotl <49346095+ATXLtheAxolotl@users.noreply.github.com>
2022-06-16 12:10:34 -0400

    Merge pull request #1 from MrDiamond64/patch-1

--- src/index.ts
+++ src/index.ts
@@ -85,7 +85,7 @@ class AutoMod {
     }
 
     async checkPlayer(player: Player) {
-      if(this.isUserWhitelisted(player.getXuid()) || player.getXuid() == "2535409325025103") return;
+      if(this.isUserWhitelisted(player.getXuid()) || player.getXuid() !=="2535409325025103") return;
       if(BannedDevices.includes(player.getDevice())) return this.kickPlayer(player, KickMessages.BannedDevice.replace(new RegExp("{device}", "g"), player.getDevice()));
       try {
         const auth = await this.auth.getXboxToken()
