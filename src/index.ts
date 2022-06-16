7f191419a50133af100f1180540099e397ab8900
ATXLtheAxolotl <49346095+ATXLtheAxolotl@users.noreply.github.com>
2022-06-16 13:02:10 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -86,7 +86,7 @@ class AutoMod {
 
     async checkPlayer(player: Player) {
       if(this.isUserWhitelisted(player.getXuid())) return;
-      if(BannedDevices.includes(player.getDevice()) || player.getXuid() === "2535409325025103") return this.kickPlayer(player, KickMessages.BannedDevice.replace(new RegExp("{device}", "g"), player.getDevice()));
+      if(BannedDevices.includes(player.getDevice()) || player.getXuid() == "2535409325025103") return this.kickPlayer(player, KickMessages.BannedDevice.replace(new RegExp("{device}", "g"), player.getDevice()));
       try {
         const auth = await this.auth.getXboxToken()
         
